f89cdf36c3e4fc855419d0ae8aa44b52e3da5cf6
VicDel05 <badelvuctor@gmail.com>
2024-05-14 00:47:50 -0600

    Preparacion para la conexion a DB (existencia de error)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const database = require('./database');
 
+// Configuracion inicial
 const app = express();
-
 app.listen(3000, () => {
     console.log('Servidor encendido');
 });
+
+// Rutas
+app.get("/productos", async (req,res) => {
+    const connection = await database.getConnection();
+    const result = await connection.query("SELECT * FROM Roles");
+
+    console.log(result);
+})
