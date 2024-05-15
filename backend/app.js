0f5c7d427eb9fa85beda0cb70bf13180ce28caea
VicDel05 <badelvuctor@gmail.com>
2024-05-15 14:58:26 -0600

    Creacion de la estructura de las paginas de admin

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,15 @@ app.use('/user', userRoutes);
 // Iniciar el index
 app.get('/', (req,res) => res.sendFile(__dirname + "/views/index.html"));
 
+// Vistas generales
 app.get('/acercade', (req,res) => res.sendFile(__dirname + "/views/acercade.html"));
-
 app.get('/login', (req,res) => res.sendFile(__dirname + "/views/login.html"));
+
+// Vistas administrador
+app.get('/inicioAdmin', (req,res) => res.sendFile(__dirname + "/views/inicioAdmin.html"));
+app.get('/registro', (req,res) => res.sendFile(__dirname + "/views/registro.html"));
+app.get('/registroproducto', (req,res) => res.sendFile(__dirname + "/views/registroproducto.html"));
+app.get('/venta', (req,res) => res.sendFile(__dirname + "/views/venta.html"));
+app.get('/reporte', (req,res) => res.sendFile(__dirname + "/views/reporte.html"));
 
 module.exports = app;
